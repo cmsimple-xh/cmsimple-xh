3e8251ca0ba6711f6176ce21786c47a4216b078e
Leen Moerland <leen@mldwebsites.nl>
2010-11-02 17:37:54 +0000

    Updated search routine (utf-8 problem)
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@66 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/search.php
+++ cmsimple/search.php
@@ -1,29 +1,37 @@
 <?php
+
 /* utf8-marker = äöüß */
 /*
-CMSimple version 3.2 - June 20. 2008
-Small - simple - smart
-© 1999-2008 Peter Andreas Harteg - peter@harteg.dk
+  CMSimple version 3.2 - June 20. 2008
+  Small - simple - smart
+  © 1999-2008 Peter Andreas Harteg - peter@harteg.dk
 
-This file is part of CMSimple
-For licence see notice in /cmsimple/cms.php and http://www.cmsimple.org/?Licence
-*/
+  This file is part of CMSimple
+  For licence see notice in /cmsimple/cms.php and http://www.cmsimple.org/?Licence
+ */
 
-if (eregi('search.php', sv('PHP_SELF')))die('Access Denied');
+if (eregi('search.php', sv('PHP_SELF'))
+    )die('Access Denied');
 
 $title = $tx['title']['search'];
 $ta = array();
-if ($search != '')for($i = 0; $i < $cl; $i++) {
-	if (!hide($i))if (@preg_match('/'.preg_quote($search, '/').'/i', (function_exists('html_entity_decode')?html_entity_decode($c[$i]):$c[$i])))$ta[] = $i;
+if ($search != ''
+    )for ($i = 0; $i < $cl; $i++) {
+        if (!hide($i)) {
+            if (@preg_match('/' . preg_quote(mb_strtolower($search, $tx['meta']['codepage']), '/') . '/i', (function_exists('html_entity_decode') ? html_entity_decode(mb_strtolower($c[$i], $tx['meta']['codepage']), ENT_QUOTES, $tx['meta']['codepage']) : mb_strtolower($c[$i], $tx['meta']['codepage'])))) {
+                $ta[] = $i;
+            }
+        }
+    }
+$o .= '<h1>' . $tx['search']['result'] . '</h1><p>"' . htmlspecialchars(stsl($search)) . '" ';
+if (count($ta) == 0
+    )$o .= $tx['search']['notfound'] . '.</p>';
+else {
+    $o .= $tx['search']['foundin'] . ' ' . count($ta) . ' ';
+    if (count($ta) > 1
+        )$o .= $tx['search']['pgplural'];
+    else
+        $o .= $tx['search']['pgsingular'];
+    $o .= ':</p>' . li($ta, 'search');
 }
-$o .= '<h1>'.$tx['search']['result'].'</h1><p>"'.htmlspecialchars(stsl($search)).'" ';
-if (count($ta) == 0)$o .= $tx['search']['notfound'].'.</p>';
-else
-	{
-	$o .= $tx['search']['foundin'].' '.count($ta). ' ';
-	if (count($ta) > 1)$o .= $tx['search']['pgplural'];
-	else $o .= $tx['search']['pgsingular'];
-	$o .= ':</p>'.li($ta, 'search');
-}
-
 ?>
