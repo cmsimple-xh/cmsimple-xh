8d47694c0850c73cd48f66e4cbe35516f9e43ef4
Martin Sereday <info@cmsimple.sk>
2010-11-18 19:17:31 +0000

    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@72 4f8f9682-67af-44ce-aacd-401fd16d96b5

---
+++ FCKeditor/editor/plugins/sitelink/lang/sk.js
@@ -0,0 +1,2 @@
+﻿FCKLang["sitelinkTitle"] = 'Vnútorný odkaz';
+FCKLang["sitelinkNotselected"] = 'Nič nie je označené!';
---
+++ FCKeditor/editor/plugins/swfobject/lang/sk.js
@@ -0,0 +1,11 @@
+FCKLang.DlgFlashvars			= 'Flash premennÈ';
+
+FCKLang.DlgFlashScriptaccess = 'Script Access';
+FCKLang.DlgFlashAlways = 'Vûdy';
+FCKLang.DlgFlashSameDomain = 'Rovnak· domÈna';
+FCKLang.DlgFlashNever = 'Nikdy';
+FCKLang.DlgFlashWindowMode = 'MÛd okna';
+FCKLang.DlgFlashWindow = 'Okno';
+FCKLang.DlgFlashOpaque = 'NepriehæadnÈ';
+FCKLang.DlgFlashTransparent = 'PriehæadnÈ';
+FCKLang.DlgFlashAllowfullscreen = 'Povoliù cel˙ obrazovku';
