563112081b9c7b0f31976e8723bd1ac214058a1b
agaida <agaida@siduction.org>
2011-05-23 10:24:21 +0000

    Fixes #3149759 - Tracker sf Fixes #3164440 - Tracker sf Fixes #14 - plan.io Fixes #16 - plan.io Variables in huconfig.php gefixt
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@104 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/languages/huconfig.php
+++ cmsimple/languages/huconfig.php
@@ -1,9 +1,9 @@
 <?php
 /* utf8-marker = äöüß */
-$tx['site']['title']="A Németországban élő magyar kontemplatív domonkos nővérek oldala";
-$tx['meta']['keywords']="domonkos rend, kontemplatív domonkos nővérek, szemlélődő, Szent Domonkos";
-$tx['meta']['description']="Leírás magyarul - megváltoztatni a nyelvi beállítások között lehet";
-$tx['template']['text1']="template-text 1";
-$tx['template']['text2']="template-text 2";
-$tx['template']['text3']="template-text 3";
+$txc['site']['title']="A Németországban élő magyar kontemplatív domonkos nővérek oldala";
+$txc['meta']['keywords']="domonkos rend, kontemplatív domonkos nővérek, szemlélődő, Szent Domonkos";
+$txc['meta']['description']="Leírás magyarul - megváltoztatni a nyelvi beállítások között lehet";
+$txc['template']['text1']="template-text 1";
+$txc['template']['text2']="template-text 2";
+$txc['template']['text3']="template-text 3";
 ?>
