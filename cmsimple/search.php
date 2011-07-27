e6bbbeb7cd90fa9df4b04da7d6d5a38d691fead3
Martin Damken <md@zeichenkombinat.de>
2011-07-27 18:28:33 +0000

    changes: ignore html tags and CMSimple-scripting, search $output - simple AND search
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@123 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/search.php
+++ cmsimple/search.php
@@ -2,7 +2,7 @@
 
 /* utf8-marker = äöüß */
 /*
-  CMSimple version 1.4.1 - June 27. 2011
+  CMSimple version 1.5 beta - Aug. 2011
   Small - simple - smart
   © 1999-2008 Peter Andreas Harteg - peter@harteg.dk
 
@@ -10,28 +10,81 @@
   For licence see notice in /cmsimple/cms.php and http://www.cmsimple.org/?Licence
  */
 
-if (eregi('search.php', sv('PHP_SELF'))
-    )die('Access Denied');
+if (eregi('search.php', sv('PHP_SELF'))) {
+    die('Access Denied');
+}
+
+if(!function_exists('mb_strtolower')) {
+    function mb_strtolower($string, $charset = null) {
+        $string = utf8_decode($string);
+        $string = strtolower($string);
+        $string = utf8_encode($string);
+        return $string;
+    }
+}
+
+
 
 $title = $tx['title']['search'];
 $ta = array();
-if ($search != ''
-    )for ($i = 0; $i < $cl; $i++) {
+if ($search != '') {
+    $search = mb_strtolower(trim($search), 'utf-8');
+    $words = explode(' ', $search);
+
+    foreach ($c as $i => $pagexyz) {
         if (!hide($i)) {
-            if (@preg_match('/' . preg_quote(mb_strtolower($search, $tx['meta']['codepage']), '/') . '/i', (function_exists('html_entity_decode') ? html_entity_decode(mb_strtolower($c[$i], $tx['meta']['codepage']), ENT_QUOTES, $tx['meta']['codepage']) : mb_strtolower($c[$i], $tx['meta']['codepage'])))) {
-                $ta[] = $i;
+            $found  = true;
+            preCallPlugins($i);
+            $pagexyz = mb_strtolower(strip_tags($pagexyz), 'utf-8');
+            $pagexyz = html_entity_decode($pagexyz, ENT_QUOTES, 'utf-8') ;
+
+            preg_match("~".$cf['scripting']['regexp']."~is",  $pagexyz, $matches);
+            if(count($matches) > 0) {
+                $pagexyz = str_replace($matches[0], '', $pagexyz);
+                if(trim($matches[1]) !== 'hide' && trim($matches[1]) !== 'remove') {
+		    $output = '';
+		    $o = '';
+                    @eval($matches[1]);
+                    $pagexyz .= $output;
+                    $pagexyz .= $o;
+                }
             }
+            foreach($words as $word) {
+                if(strpos($pagexyz, trim($word)) === false) {
+                    $found = false;
+                    break;
+                }
+            }
+            if (!$found) {
+                continue;
+            }
+            $ta[] = $i;
+
         }
     }
+    if(count($ta) > 0){
+        $cms_searchresults = "\n" .'<ul>';
+	
+	$words = (implode( ",", $words));
+        foreach($ta as $i){
+            $cms_searchresults .= "\n\t" . '<li><a href="' . $sn . '?' . $u[$i] . amp() . 'search=' . $words .'">' . $h[$i] . '</a></li>';
+        }
+        $cms_searchresults .= "\n" . '</ul>' . "\n";
+    }
+}
+
 $o .= '<h1>' . $tx['search']['result'] . '</h1><p>"' . htmlspecialchars(stsl($search)) . '" ';
-if (count($ta) == 0
-    )$o .= $tx['search']['notfound'] . '.</p>';
+
+if (count($ta) == 0) {
+    $o .= $tx['search']['notfound'] . '.</p>';
+}
 else {
     $o .= $tx['search']['foundin'] . ' ' . count($ta) . ' ';
     if (count($ta) > 1
-        )$o .= $tx['search']['pgplural'];
+    )$o .= $tx['search']['pgplural'];
     else
         $o .= $tx['search']['pgsingular'];
-    $o .= ':</p>' . li($ta, 'search');
+    $o .= ':</p>' . $cms_searchresults;
 }
+
 ?>
