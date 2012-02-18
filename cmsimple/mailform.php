2c4de1d5caa93aee36a4b0f54e486f755dcc8713
Gert Ebersbach <mail@ge-webdesign.de>
2012-02-18 08:49:47 +0000

    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@171 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/mailform.php
+++ cmsimple/mailform.php
@@ -22,6 +22,7 @@
  
 /* 
 History:
+2012-02-18  GE removed inline css to core.css, added outer div with id for better styling by template
 2011-08-30  captcha on/off + change in error messages by svasti, code improvement by cmb
 2010-06-12  Bob for XH 1.2 : Mail header subject localized
 2009-09-18  GE for CMSimple_XH
@@ -30,6 +31,7 @@ History:
 
 if (preg_match('/mailform.php/i',sv('PHP_SELF')))die('Access Denied');
 
+$o .= "\n" . '<div id="cmsimple_mailform">' .  "\n";
 $title = $tx['title'][$f];
 $o .= '<h1>' . $title . '</h1>';
 initvar('sendername');
@@ -43,35 +45,32 @@ $t = '';
 
 if ($action == 'send')
 {
-    $msg = ($tx['mailform']['sendername'] . ": "
-         . stsl($sendername) . "\n"
-         . $tx['mailform']['senderphone'] . ": "
-         . stsl($senderphone) . "\n\n" . stsl($mailform));
+	$msg = ($tx['mailform']['sendername'] . ": "
+	. stsl($sendername) . "\n"
+	. $tx['mailform']['senderphone'] . ": "
+	. stsl($senderphone) . "\n\n" . stsl($mailform));
 
 // echo ($msg);
-    if ($getlast != $cap && trim($cf['mailform']['captcha']) == 'true')
-    {
-        $e .= '<li style="font-size: 118%;">' . $tx['mailform']['captchafalse'];
-    }
-    if ($mailform == '')
-    {
-        $e .= '<li style="font-size: 118%;">' . $tx['mailform']['mustwritemessage'];
-    }
-    if (!(preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[_a-z0-9-]+(\.[_a-z0-9-]+)*(\.([a-z]{2,4}))+$/i", $sender)))
-    {
-        $e .= '<li style="font-size: 118%;">' . $tx['mailform']['notaccepted'];
-    }
-    if (!$e
-        && !(@mail_utf8($cf['mailform']['email'],
-           $tx['menu']['mailform'] . ' ' . sv('SERVER_NAME'),
-           $msg, "From: " . stsl($sender) . "\r\n" . "X-Remote: " . sv('REMOTE_ADDR') . "\r\n")))
-    {
-        $e .= '<li style="font-size: 118%;">' . $tx['mailform']['notsend'];
-    }
-    else
-    {
-        $t = '<p style="font-size: 118%; text-align: center;">' . $tx['mailform']['send'] . '</p>';
-    }
+	if ($getlast != $cap && trim($cf['mailform']['captcha']) == 'true')
+	{
+		$e .= '<li>' . $tx['mailform']['captchafalse'] . '</li>';
+	}
+	if ($mailform == '')
+	{
+		$e .= '<li>' . $tx['mailform']['mustwritemessage'] . '</li>';
+	}
+	if (!(preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[_a-z0-9-]+(\.[_a-z0-9-]+)*(\.([a-z]{2,4}))+$/i", $sender)))
+	{
+		$e .= '<li>' . $tx['mailform']['notaccepted'] . '</li>';
+	}
+	if (!$e && !(@mail_utf8($cf['mailform']['email'], $tx['menu']['mailform'] . ' ' . sv('SERVER_NAME'), $msg, "From: " . stsl($sender) . "\r\n" . "X-Remote: " . sv('REMOTE_ADDR') . "\r\n")))
+	{
+		$e .= '<li>' . $tx['mailform']['notsend'] . '</li>';
+	}
+	else
+	{
+		$t = '<p>' . $tx['mailform']['send'] . '</p>';
+	}
 }
 
 if ($t == '' || $e != '')
@@ -82,60 +81,60 @@ if ($t == '' || $e != '')
 	srand((double)microtime()*1000000);
 	$random=rand(10000,99999);
 
-	$o .= '<form action="'.$sn.'" method="post">';
+	$o .= '<form action="'.$sn.'" method="post">' . "\n";
 
-	$o .= tag('input type="hidden" name="function" value="mailform"');
+	$o .= tag('input type="hidden" name="function" value="mailform"') . "\n";
 
-    if (trim($cf['mailform']['captcha']) == 'true')
-    {
-        $o .= tag('input type="hidden" name="getlast" value="'.$random.'"');
-    }
-	$o .= tag('input type="hidden" name="action" value="send"');
+	if (trim($cf['mailform']['captcha']) == 'true')
+	{
+		$o .= tag('input type="hidden" name="getlast" value="'.$random.'"') . "\n";
+	}
+	$o .= tag('input type="hidden" name="action" value="send"') . "\n";
 
 // fields before textarea 
-	$o .= '<div style="width:200px; margin: 0 0 8px 0;">'.$tx['mailform']['sendername'].': '
-	   .  tag('input type="text" class="text" size="35" name="sendername" value="'
-       .  htmlspecialchars(stsl($sendername)).'"')
-	   .  '</div>'
-	   .  '<div style="width:200px; margin: 0 0 8px 0;">'.$tx['mailform']['senderphone'].': '
-	   .  tag('input type="text" class="text"  size="35"name="senderphone" value="'
-       .  htmlspecialchars(stsl($senderphone)).'"').'</div>'
-	   .  '<div style="width:200px; margin: 0 0 20px 0;">'
-       .  $tx['mailform']['sender'].': '
-       .  tag('input type="text" class="text"  size="35" name="sender" value="'
-       .  htmlspecialchars(stsl($sender)).'"').'</div>';
+	$o .= '<div>' . "\n" . $tx['mailform']['sendername'].': ' . tag('br') . "\n"
+	.  tag('input type="text" class="text" size="35" name="sendername" value="'
+	.  htmlspecialchars(stsl($sendername)).'"') . "\n"
+	.  '</div>' . "\n"
+	.  '<div>' . "\n" . $tx['mailform']['senderphone'].': ' . tag('br') . "\n"
+	.  tag('input type="text" class="text" size="35"name="senderphone" value="'
+	.  htmlspecialchars(stsl($senderphone)).'"') . "\n"
+	. '</div>' . "\n"
+	.  '<div>' . "\n" .  $tx['mailform']['sender'].': ' . tag('br') . "\n"
+	.  tag('input type="text" class="text" size="35" name="sender" value="'
+	.  htmlspecialchars(stsl($sender)).'"') . "\n"
+	. '</div>' . "\n" . tag('br') . "\n";
 
 // textarea
-	$o .= '<textarea style="border-color:#6a6a6a !important; border-width:1px;" rows="12" cols="40" name="mailform">';
-	if ($mailform != 'true') $o .= htmlspecialchars(stsl($mailform));
-	$o .= '</textarea>';
+	$o .= '<textarea rows="12" cols="40" name="mailform">' . "\n";
+	if ($mailform != 'true') $o .= htmlspecialchars(stsl($mailform)) . "\n";
+	$o .= '</textarea>' . "\n";
 
 // captcha
     if (trim($cf['mailform']['captcha']) == 'true')
-    {
-    	$o .= '<div class="captcha" style="margin: 10px 0;">'
-           .  tag('input style="float:left" type="text" name="cap" class="captchainput"')
-           .  '<div style="float:left; color: #fff; text-decoration: none; background-color: #000; padding: 2px 5px; margin: 0px 4px;">'
-           .  $random . '</div><div style="float:left; margin-left:5px; margin-top:2px;">'
-           .  $tx['mailform']['captcha'] .'</div></div>';
+	{
+		$o .= '<p>' .  $tx['mailform']['captcha'] . '</p>' . "\n"
+		. tag('input type="text" name="cap" class="captchainput"') . "\n"
+		.  '<span class="captcha_code">' . "\n"
+		.  $random . '</span>' . "\n";
     }
 
 // sendbutton
-	$o .= '<div style="clear:both; padding: 8px 0;">'
-       .  tag('input type="submit" class="submit" value="'
-       .  $tx['mailform']['sendbutton'] . '"') . '</div>';
-	$o .= '</form>';
+	$o .= '<div style="clear: both;">' . "\n"
+	.  tag('input type="submit" class="submit" value="'
+	.  $tx['mailform']['sendbutton'] . '"') . "\n" . '</div>' . "\n";
+	$o .= '</form>' . "\n" . '</div>' . "\n";
 
 }
 else $o .= $t;
 
 function mail_utf8($to, $subject = '(No Subject)', $message = '', $header = '')
 {
-    $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n";
-    if(mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header))
-    {
-        return true;
-    }
-    return false;
+	$header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n";
+	if(mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header))
+	{
+		return true;
+	}
+	return false;
 }
 ?>
