ffb8fcaf0a04a200304bee1888d2be361497927f
Gert Ebersbach <mail@ge-webdesign.de>
2012-02-20 20:32:32 +0000

    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@182 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/adm.php
+++ cmsimple/adm.php
@@ -383,15 +383,22 @@ if ($adm) {
                                         else if ($k1 . $k2 == 'subsitetemplate')
                                             selectlist('templates', "/^[^\.]*$/i", "/^([^\.]*)$/i"); // for subsites
 
+// use input fields only in CMS config
+                                        
+                                        else if ($a == 'cf')
+                                        {
+                                            $o .= tag('input type="text" class="text" name="'.$k1.'_'.$k2.'" value="'.$v2.'" size="30"')."\n";
+                                        }
+
 //new in 1.5: single line input field or textarea depending on text length
-											else if (strlen($v2) < 30)
-											{
-												$o .= '<textarea rows="2" cols="30" class="cmsimplecore_settings cmsimplecore_settings_short" name="' . $k1 . '_' . $k2 . '">' . $v2 . "</textarea>\n";
-											}
-											else
-											{
+										else if (strlen($v2) < 30)
+										{
+											$o .= '<textarea rows="2" cols="30" class="cmsimplecore_settings cmsimplecore_settings_short" name="' . $k1 . '_' . $k2 . '">' . $v2 . "</textarea>\n";
+										}
+										else
+										{
 												$o .= '<textarea rows="2" cols="30" class="cmsimplecore_settings" name="' . $k1 . '_' . $k2 . '">' . $v2 . "</textarea>\n";
-											}
+										}
                                         $o .= '</td>' . "\n" . '</tr>' . "\n";
                                     }
                         }
