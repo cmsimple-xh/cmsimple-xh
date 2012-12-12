1e7519afa48d11b389b821c0c913293b4d3641b8
manu37 <marinello@pixolution.ch>
2012-12-12 15:01:33 +0000

    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@385 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- plugins/tinymce/admin.php
+++ plugins/tinymce/admin.php
@@ -13,8 +13,8 @@ if ($tinymce) {
     $o = '<div class="plugintext">';
     $o .= '<div class="plugineditcaption">TinyMCE for CMSimple_XH</div>';
     $o .= '<p>Version for $CMSIMPLE_XH_VERSION$</p>';
-    $o .= '<p>TinyMCE version 3.5.6  &ndash; <a href="http://www.tinymce.com/" target="_blank">http://www.tinymce.com/</a></p>';
-    $o .= '<p>CMSimpe_xh & Filebrowser integration &ndash; <a href="http://www.tinymce.com/" target="_blank">http://www.zeichenkombinat.de/</a></p>';
+    $o .= '<p>TinyMCE version 3.5.8  &ndash; <a href="http://www.tinymce.com/" target="_blank">http://www.tinymce.com/</a></p>';
+    $o .= '<p>CMSimpe_xh & Filebrowser integration &ndash; <a href="http://www.pixolution.ch/" target="_blank">http://www.pixolution.ch/</a></p>';
 
 
 
