3ad5ab21557445bc175472fb680f23c2db22d1fd
manu37 <marinello@pixolution.ch>
2013-01-04 13:09:32 +0000

    li():Wrap active menu title in <span>.
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@417 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/tplfuncs.php
+++ cmsimple/tplfuncs.php
@@ -148,84 +148,76 @@ function toc($start = null, $end = null, $li = 'li')
  * @param mixed $st  The menu level to start with or the type of menu.
  * @return string  The (X)HTML.
  */
-function li($ta, $st)
+function li($ta, $st) 
 {
-    global $s, $l, $h, $cl, $cf, $u;
-
-    $tl = count($ta);
-    if ($tl < 1) {
-        return;
-    }
-    $t = '';
-    if ($st == 'submenu' || $st == 'search') {
-        $t .= '<ul class="' . $st . '">' . "\n";
-    }
-    $b = 0;
-    if ($st > 0) {
-        $b = $st - 1;
-        $st = 'menulevel';
-    }
-    $lf = array();
-    for ($i = 0; $i < $tl; $i++) {
-        $tf = ($s != $ta[$i]);
-        if ($st == 'menulevel' || $st == 'sitemaplevel') {
-            for ($k = (isset($ta[$i - 1]) ? $l[$ta[$i - 1]] : $b); $k < $l[$ta[$i]]; $k++) {
-                $t .= "\n" . '<ul class="' . $st . ($k + 1) . '">' . "\n";
-            }
-        }
-        $t .= '<li class="';
-        if (!$tf) {
-            $t .= 's';
-        } else if (@$cf['menu']['sdoc'] == "parent" && $s > -1) {
-            if ($l[$ta[$i]] < $l[$s]) {
-                if (@substr($u[$s], 0, 1 + strlen($u[$ta[$i]])) == $u[$ta[$i]] . $cf['uri']['seperator']) {
-                    $t .= 's';
-                }
-            }
-        }
-        $t .= 'doc';
-        for ($j = $ta[$i] + 1; $j < $cl; $j++) {
-            if (!hide($j) && $l[$j] - $l[$ta[$i]] < 2 + $cf['menu']['levelcatch']) {
-                if ($l[$j] > $l[$ta[$i]]) {
-                    $t .= 's';
-                }
-                break;
-            }
-        }
-        $t .= '">';
-        if ($tf) {
-            $t .= a($ta[$i], '');
-        }
-        $t .= $h[$ta[$i]];
-        if ($tf) {
-            $t .= '</a>';
-        }
-        if ($st == 'menulevel' || $st == 'sitemaplevel') {
-            if ((isset($ta[$i + 1]) ? $l[$ta[$i + 1]] : $b) > $l[$ta[$i]]) {
-                $lf[$l[$ta[$i]]] = true;
-            } else {
-                $t .= '</li>' . "\n";
-                $lf[$l[$ta[$i]]] = false;
-            }
-            for ($k = $l[$ta[$i]]; $k > (isset($ta[$i + 1]) ? $l[$ta[$i + 1]] : $b); $k--) {
-                $t .= '</ul>' . "\n";
-                if (isset($lf[$k - 1])) {
-                    if ($lf[$k - 1]) {
-                        $t .= '</li>' . "\n";
-                        $lf[$k - 1] = false;
-                    }
-                }
-            }
-        } else {
-            $t .= '</li>' . "\n";
-        }
-    }
-    if ($st == 'submenu' || $st == 'search') {
-        $t .= '</ul>' . "\n";
-    }
-    return $t;
+		global $s, $l, $h, $cl, $cf, $u;
+		$tl = count($ta);
+		if ($tl < 1)
+				return;
+		$t = '';
+		if ($st == 'submenu' || $st == 'search')
+				$t .= '<ul class="' . $st . '">' . "\n";
+		$b = 0;
+		if ($st > 0) {
+				$b = $st - 1;
+				$st = 'menulevel';
+		}
+		$lf = array();
+		for ($i = 0; $i < $tl; $i++) {
+				$tf = ($s != $ta[$i]);
+				if ($st == 'menulevel' || $st == 'sitemaplevel') {
+						for ($k = (isset($ta[$i - 1]) ? $l[$ta[$i - 1]] : $b); $k < $l[$ta[$i]]; $k++)
+								$t .= "\n" . '<ul class="' . $st . ($k + 1) . '">' . "\n";
+				}
+				$t .= '<li class="';
+				if (!$tf)
+						$t .= 's';
+				else if (@$cf['menu']['sdoc'] == "parent" && $s > -1) {
+						if ($l[$ta[$i]] < $l[$s]) {
+								if (@substr($u[$s], 0, 1 + strlen($u[$ta[$i]])) == $u[$ta[$i]] . $cf['uri']['seperator'])
+										$t .= 's';
+						}
+				}
+				$t .= 'doc';
+				for ($j = $ta[$i] + 1; $j < $cl; $j++)
+						if (!hide($j) && $l[$j] - $l[$ta[$i]] < 2 + $cf['menu']['levelcatch']) {
+								if ($l[$j] > $l[$ta[$i]])
+										$t .= 's';
+								break;
+						}
+				$t .= '">';
+				if ($tf)
+						$t .= a($ta[$i], '');
+				else            				//EM+
+						$t .='<span>';      //EM+
+				$t .= $h[$ta[$i]];
+				if ($tf)
+						$t .= '</a>';   
+				else            			//EM+
+						$t .='</span>';   //EM+
+				if ($st == 'menulevel' || $st == 'sitemaplevel') {
+						if ((isset($ta[$i + 1]) ? $l[$ta[$i + 1]] : $b) > $l[$ta[$i]])
+								$lf[$l[$ta[$i]]] = true;
+						else {
+								$t .= '</li>' . "\n";
+								$lf[$l[$ta[$i]]] = false;
+						}
+						for ($k = $l[$ta[$i]]; $k > (isset($ta[$i + 1]) ? $l[$ta[$i + 1]] : $b); $k--) {
+								$t .= '</ul>' . "\n";
+								if (isset($lf[$k - 1]))
+										if ($lf[$k - 1]) {
+												$t .= '</li>' . "\n";
+												$lf[$k - 1] = false;
+										}
+						};
+				}
+				else
+						$t .= '</li>' . "\n";
+		}
+		if ($st == 'submenu' || $st == 'search')
+				$t .= '</ul>' . "\n";
+		return $t;
 }
-
 
 /**
  * Returns the search form.
