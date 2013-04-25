8f45cc83c88899631f663057543e15f60b365a42
cmb69 <cmbecker69@gmx.de>
2013-04-25 17:42:10 +0000

    fixed minor bug, where id="login" was used twice for the login form
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@527 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/login.php
+++ cmsimple/login.php
@@ -94,7 +94,7 @@ function loginforms() {
         $cf['meta']['robots'] = "noindex";
         $onload .= "self.focus();document.login.passwd.focus();";
         $f = $tx['menu']['login'];
-        $o .= '<h1>' . $tx['menu']['login'] . '</h1><p><b>' . $tx['login']['warning'] . '</b></p><form id="login" name="login" action="' . $sn . '?' . $u[$s] . '" method="post"><div id="login">' . tag('input type="hidden" name="login" value="true"') . tag('input type="hidden" name="selected" value="' . @$u[$s] . '"') . tag('input type="password" name="passwd" id="passwd" value=""') . ' ' . tag('input type="submit" name="submit" id="submit" value="' . $tx['menu']['login'] . '"') . '</div></form>';
+        $o .= '<h1>' . $tx['menu']['login'] . '</h1><p><b>' . $tx['login']['warning'] . '</b></p><form id="login" name="login" action="' . $sn . '?' . $u[$s] . '" method="post">' . tag('input type="hidden" name="login" value="true"') . tag('input type="hidden" name="selected" value="' . @$u[$s] . '"') . tag('input type="password" name="passwd" id="passwd" value=""') . ' ' . tag('input type="submit" name="submit" id="submit" value="' . $tx['menu']['login'] . '"') . '</form>';
         $s = -1;
     }
 }
