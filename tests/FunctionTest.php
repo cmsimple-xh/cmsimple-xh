afcfaf69389f7bad7dde2923f27bc6699791b821
cmb69 <cmbecker69@gmx.de>
2013-04-30 12:09:41 +0000

    added unit tests for amp()
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@539 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -10,6 +10,14 @@ include '../cmsimple/functions.php';
 
 class FunctionTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @expectedException PHPUnit_Framework_Error_Deprecated
+     */
+    public function testRpIsDeprecated()
+    {
+        rp('');
+    }
+
     public function dataForTestRp()
     {
         return array(
@@ -19,19 +27,39 @@ class FunctionTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException PHPUnit_Framework_Error_Deprecated
-     */
-    public function testRpIsDeprecated()
-    {
-        rp('');
-    }
-
-    /**
      * @dataProvider dataForTestRp
      */
     public function testRp($filename, $expected)
     {
         $actual = @rp($filename); // suppress deprecated warning
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Deprecated
+     */
+    public function testAmpIsDeprecated()
+    {
+        amp();
+    }
+
+    public function dataForTestAmp()
+    {
+        return array(
+            array('true', '&amp;'),
+            array('', '&')
+        );
+    }
+
+    /**
+     * @dataProvider dataForTestAmp()
+     */
+    public function testAmp($xhtmlAmp, $expected)
+    {
+        global $cf;
+
+        $cf['xhtml']['amp'] = $xhtmlAmp;
+        $actual = @amp();
         $this->assertEquals($expected, $actual);
     }
 }
