8dedf951b1f47201e8bb3ff17489454048605319
cmb69 <cmbecker69@gmx.de>
2013-07-09 19:29:48 +0000

    fixed regression bug (r366), where line number was not displayed for the location where the headers were sent
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@715 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/cms.php
+++ cmsimple/cms.php
@@ -1185,7 +1185,7 @@ if ($title == '') {
 if (!headers_sent($temp, $i)) {
     header('Content-Type: text/html; charset=UTF-8');
 } else {
-    $temp .= ':' . $$i;
+    $temp .= ':' . $i;
     exit(str_replace('{location}', $temp, $tx['error']['headers']));
 }
 
