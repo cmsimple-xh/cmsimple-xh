1d027ea9c93026d46f94efc62dd7e1fda9311375
cmb69 <cmbecker69@gmx.de>
2013-08-14 18:06:56 +0000

    added demo pages for the different templates
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@860 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- content/pagedata.php
+++ content/pagedata.php
@@ -34,7 +34,7 @@ $temp_data['use_header_location'] = '';
 
 ################## Page Data ############
 $page_data[0]['url'] = 'Welcome_to_CMSimple_XH';
-$page_data[0]['last_edit'] = '1331835803';
+$page_data[0]['last_edit'] = '1376494820';
 $page_data[0]['description'] = '';
 $page_data[0]['keywords'] = '';
 $page_data[0]['title'] = '';
@@ -258,7 +258,7 @@ $page_data[13]['use_header_location'] = '';
 
 //----------
 $page_data[14]['url'] = 'Templates_%26amp%3B_Plugins';
-$page_data[14]['last_edit'] = '1320934341';
+$page_data[14]['last_edit'] = '1376503356';
 $page_data[14]['description'] = '';
 $page_data[14]['keywords'] = '';
 $page_data[14]['title'] = '';
@@ -273,8 +273,8 @@ $page_data[14]['header_location'] = '';
 $page_data[14]['use_header_location'] = '';
 
 //----------
-$page_data[15]['url'] = 'Languages';
-$page_data[15]['last_edit'] = '1329938196';
+$page_data[15]['url'] = 'mini1';
+$page_data[15]['last_edit'] = '1376503493';
 $page_data[15]['description'] = '';
 $page_data[15]['keywords'] = '';
 $page_data[15]['title'] = '';
@@ -289,52 +289,180 @@ $page_data[15]['header_location'] = '';
 $page_data[15]['use_header_location'] = '';
 
 //----------
-$page_data[16]['url'] = 'News01';
-$page_data[16]['last_edit'] = '1316870463';
+$page_data[16]['url'] = 'n6200tbisGPL3';
+$page_data[16]['last_edit'] = '1376500915';
 $page_data[16]['description'] = '';
 $page_data[16]['keywords'] = '';
 $page_data[16]['title'] = '';
 $page_data[16]['robots'] = '';
 $page_data[16]['heading'] = '';
 $page_data[16]['show_heading'] = '0';
-$page_data[16]['template'] = '0';
+$page_data[16]['template'] = 'n6200tbisGPL3';
 $page_data[16]['published'] = '1';
 $page_data[16]['show_last_edit'] = '0';
-$page_data[16]['linked_to_menu'] = '0';
+$page_data[16]['linked_to_menu'] = '1';
 $page_data[16]['header_location'] = '';
 $page_data[16]['use_header_location'] = '0';
 
 //----------
-$page_data[17]['url'] = 'News02';
-$page_data[17]['last_edit'] = '1316870483';
+$page_data[17]['url'] = 'photo11';
+$page_data[17]['last_edit'] = '1376501412';
 $page_data[17]['description'] = '';
 $page_data[17]['keywords'] = '';
 $page_data[17]['title'] = '';
 $page_data[17]['robots'] = '';
 $page_data[17]['heading'] = '';
 $page_data[17]['show_heading'] = '0';
-$page_data[17]['template'] = '0';
+$page_data[17]['template'] = 'photo11';
 $page_data[17]['published'] = '1';
 $page_data[17]['show_last_edit'] = '0';
-$page_data[17]['linked_to_menu'] = '0';
+$page_data[17]['linked_to_menu'] = '1';
 $page_data[17]['header_location'] = '';
 $page_data[17]['use_header_location'] = '0';
 
 //----------
-$page_data[18]['url'] = 'News03';
-$page_data[18]['last_edit'] = '1319015680';
+$page_data[18]['url'] = 'praia';
+$page_data[18]['last_edit'] = '1376501507';
 $page_data[18]['description'] = '';
 $page_data[18]['keywords'] = '';
 $page_data[18]['title'] = '';
 $page_data[18]['robots'] = '';
 $page_data[18]['heading'] = '';
 $page_data[18]['show_heading'] = '0';
-$page_data[18]['template'] = '0';
+$page_data[18]['template'] = 'praia';
 $page_data[18]['published'] = '1';
 $page_data[18]['show_last_edit'] = '0';
-$page_data[18]['linked_to_menu'] = '0';
+$page_data[18]['linked_to_menu'] = '1';
 $page_data[18]['header_location'] = '';
 $page_data[18]['use_header_location'] = '0';
+
+//----------
+$page_data[19]['url'] = 'responsivehtml';
+$page_data[19]['last_edit'] = '1376501802';
+$page_data[19]['description'] = '';
+$page_data[19]['keywords'] = '';
+$page_data[19]['title'] = '';
+$page_data[19]['robots'] = '';
+$page_data[19]['heading'] = '';
+$page_data[19]['show_heading'] = '0';
+$page_data[19]['template'] = 'responsivehtml';
+$page_data[19]['published'] = '1';
+$page_data[19]['show_last_edit'] = '0';
+$page_data[19]['linked_to_menu'] = '1';
+$page_data[19]['header_location'] = '';
+$page_data[19]['use_header_location'] = '0';
+
+//----------
+$page_data[20]['url'] = 'structure1_black';
+$page_data[20]['last_edit'] = '1376502096';
+$page_data[20]['description'] = '';
+$page_data[20]['keywords'] = '';
+$page_data[20]['title'] = '';
+$page_data[20]['robots'] = '';
+$page_data[20]['heading'] = '';
+$page_data[20]['show_heading'] = '0';
+$page_data[20]['template'] = 'structure1_black';
+$page_data[20]['published'] = '1';
+$page_data[20]['show_last_edit'] = '0';
+$page_data[20]['linked_to_menu'] = '1';
+$page_data[20]['header_location'] = '';
+$page_data[20]['use_header_location'] = '0';
+
+//----------
+$page_data[21]['url'] = 'Languages';
+$page_data[21]['last_edit'] = '1329938196';
+$page_data[21]['description'] = '';
+$page_data[21]['keywords'] = '';
+$page_data[21]['title'] = '';
+$page_data[21]['robots'] = '';
+$page_data[21]['heading'] = '';
+$page_data[21]['show_heading'] = '';
+$page_data[21]['template'] = '';
+$page_data[21]['published'] = '';
+$page_data[21]['show_last_edit'] = '';
+$page_data[21]['linked_to_menu'] = '1';
+$page_data[21]['header_location'] = '';
+$page_data[21]['use_header_location'] = '';
+
+//----------
+$page_data[22]['url'] = 'News01';
+$page_data[22]['last_edit'] = '1316870463';
+$page_data[22]['description'] = '';
+$page_data[22]['keywords'] = '';
+$page_data[22]['title'] = '';
+$page_data[22]['robots'] = '';
+$page_data[22]['heading'] = '';
+$page_data[22]['show_heading'] = '0';
+$page_data[22]['template'] = '0';
+$page_data[22]['published'] = '1';
+$page_data[22]['show_last_edit'] = '0';
+$page_data[22]['linked_to_menu'] = '0';
+$page_data[22]['header_location'] = '';
+$page_data[22]['use_header_location'] = '0';
+
+//----------
+$page_data[23]['url'] = 'News02';
+$page_data[23]['last_edit'] = '1316870483';
+$page_data[23]['description'] = '';
+$page_data[23]['keywords'] = '';
+$page_data[23]['title'] = '';
+$page_data[23]['robots'] = '';
+$page_data[23]['heading'] = '';
+$page_data[23]['show_heading'] = '0';
+$page_data[23]['template'] = '0';
+$page_data[23]['published'] = '1';
+$page_data[23]['show_last_edit'] = '0';
+$page_data[23]['linked_to_menu'] = '0';
+$page_data[23]['header_location'] = '';
+$page_data[23]['use_header_location'] = '0';
+
+//----------
+$page_data[24]['url'] = 'News03';
+$page_data[24]['last_edit'] = '1319015680';
+$page_data[24]['description'] = '';
+$page_data[24]['keywords'] = '';
+$page_data[24]['title'] = '';
+$page_data[24]['robots'] = '';
+$page_data[24]['heading'] = '';
+$page_data[24]['show_heading'] = '0';
+$page_data[24]['template'] = '0';
+$page_data[24]['published'] = '1';
+$page_data[24]['show_last_edit'] = '0';
+$page_data[24]['linked_to_menu'] = '0';
+$page_data[24]['header_location'] = '';
+$page_data[24]['use_header_location'] = '0';
+
+//----------
+$page_data[25]['url'] = 'News04';
+$page_data[25]['last_edit'] = '1319015680';
+$page_data[25]['description'] = '';
+$page_data[25]['keywords'] = '';
+$page_data[25]['title'] = '';
+$page_data[25]['robots'] = '';
+$page_data[25]['heading'] = '';
+$page_data[25]['show_heading'] = '0';
+$page_data[25]['template'] = '0';
+$page_data[25]['published'] = '1';
+$page_data[25]['show_last_edit'] = '0';
+$page_data[25]['linked_to_menu'] = '0';
+$page_data[25]['header_location'] = '';
+$page_data[25]['use_header_location'] = '0';
+
+//----------
+$page_data[26]['url'] = 'News05';
+$page_data[26]['last_edit'] = '1319015680';
+$page_data[26]['description'] = '';
+$page_data[26]['keywords'] = '';
+$page_data[26]['title'] = '';
+$page_data[26]['robots'] = '';
+$page_data[26]['heading'] = '';
+$page_data[26]['show_heading'] = '0';
+$page_data[26]['template'] = '0';
+$page_data[26]['published'] = '1';
+$page_data[26]['show_last_edit'] = '0';
+$page_data[26]['linked_to_menu'] = '0';
+$page_data[26]['header_location'] = '';
+$page_data[26]['use_header_location'] = '0';
 
 //----------
 ?>
