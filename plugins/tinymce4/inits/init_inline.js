ccfc3ce95ae63a5feaf5f6d231f64d2c693522fc
manu37 <marinello@pixolution.ch>
2013-09-03 19:39:19 +0000

    add inline init
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@920 4f8f9682-67af-44ce-aacd-401fd16d96b5

---
+++ plugins/tinymce4/inits/init_inline.js
@@ -0,0 +1,15 @@
+{
+    "selector": "textarea#text",
+    "theme": "modern",
+    "inline": true,
+    "height": 600,
+    "plugins": [
+         "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+   ],
+   "toolbar": "save undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent", 
+    "file_browser": true, 
+    "image_list": true,
+    "link_list": true
+ }
