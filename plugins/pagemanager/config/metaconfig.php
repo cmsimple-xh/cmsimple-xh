5cfe5e3dac649f0a7e310843a4e4cb6fe30bf95e
cmb69 <cmbecker69@gmx.de>
2013-09-21 13:26:50 +0000

    added metaconfig.php for Pagemanager
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@941 4f8f9682-67af-44ce-aacd-401fd16d96b5

---
+++ plugins/pagemanager/config/metaconfig.php
@@ -0,0 +1,11 @@
+<?php
+
+$plugin_mcf['pagemanager']['verbose']="bool";
+$plugin_mcf['pagemanager']['toolbar_show']="bool";
+$plugin_mcf['pagemanager']['toolbar_vertical']="bool";
+$plugin_mcf['pagemanager']['pagedata_attribute']="linked_to_menu,";
+$plugin_mcf['pagemanager']['treeview_theme']="enum:cmsimple";
+$plugin_mcf['pagemanager']['treeview_animation']="200";
+$plugin_mcf['pagemanager']['utf8_marker']="hidden";
+
+?>
