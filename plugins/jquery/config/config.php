4a16feb787bdca063b61e1f3ab943fa451c61aae
cmb69 <cmbecker69@gmx.de>
2013-11-10 23:41:20 +0000

    updated to jQuery4CMSimple 1.5
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@1078 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- plugins/jquery/config/config.php
+++ plugins/jquery/config/config.php
@@ -1,11 +1,9 @@
 <?php
 
-	$plugin_cf['jquery']['file_core']="jquery-1.9.1.min.js";
-	$plugin_cf['jquery']['version_core']="unused / deprecated";
-	$plugin_cf['jquery']['file_ui']="jquery-ui-1.10.2.min.js";
-	$plugin_cf['jquery']['version_ui']="unused / deprecated";
-	$plugin_cf['jquery']['file_migrate']="jquery-migrate-1.1.1.min.js";
-	$plugin_cf['jquery']['file_css']="smoothness/jquery-ui-1.10.2.min.css";
+	$plugin_cf['jquery']['version_core']="1.10.2";
+	$plugin_cf['jquery']['version_ui']="1.10.3";
+	$plugin_cf['jquery']['version_migrate']="jquery-migrate-1.2.1.min.js";
+	$plugin_cf['jquery']['load_migrate']="true";
 	$plugin_cf['jquery']['autoload']="";
 
 ?>
