cb64cd13cd7be626fb7b4d5f4797e71dd2ce90cf
cmb69 <cmbecker69@gmx.de>
2013-11-15 17:51:42 +0000

    removed already done @todo
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@1111 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/classes/PageDataEditor.php
+++ cmsimple/classes/PageDataEditor.php
@@ -23,7 +23,6 @@
  * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link     http://cmsimple-xh.org/
  * @since    1.6
- * @todo     add CSRF protection
  */
 class XH_PageDataEditor
 {
