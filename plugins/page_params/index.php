318088e65e38864acaee826accf956e986ef115e
cmb69 <cmbecker69@gmx.de>
2014-01-30 14:10:39 +0000

    bumped copyright year
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@1198 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- plugins/page_params/index.php
+++ plugins/page_params/index.php
@@ -14,7 +14,7 @@
  * @author    Martin Damken <kontakt@zeichenkombinat.de>
  * @author    Jerry Jakobsfeld <mail@simplesolutions.dk>
  * @author    The CMSimple_XH developers <devs@cmsimple-xh.org>
- * @copyright 2009-2013 The CMSimple_XH developers <http://cmsimple-xh.org/?The_Team>
+ * @copyright 2009-2014 The CMSimple_XH developers <http://cmsimple-xh.org/?The_Team>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @version   SVN: $Id$
  * @link      http://cmsimple-xh.org/
