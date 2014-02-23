70e26e9233f9305629c17fbcbc8570a29d3e26f1
cmb69 <cmbecker69@gmx.de>
2014-02-23 14:28:47 +0000

    updated to jQuery4CMSimple 1.5.1
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.5@1224 4f8f9682-67af-44ce-aacd-401fd16d96b5

---
+++ plugins/jquery/config/metaconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$plugin_mcf['jquery']['version_core']="function:jquery_getCoreVersions";
+$plugin_mcf['jquery']['version_ui']="function:jquery_getUiVersions";
+$plugin_mcf['jquery']['version_migrate']="function:jquery_getMigrateVersions";
+$plugin_mcf['jquery']['load_migrate']="bool";
+$plugin_mcf['jquery']['autoload']="bool";
+
+?>
