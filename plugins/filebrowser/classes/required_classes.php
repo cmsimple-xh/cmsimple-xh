dcd3ddaab9b3e8682cacc549c0ec6c01ed915a05
cmb69 <cmbecker69@gmx.de>
2014-04-24 11:05:39 +0000

    added direct access prevention
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.5@1250 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- plugins/filebrowser/classes/required_classes.php
+++ plugins/filebrowser/classes/required_classes.php
@@ -4,6 +4,11 @@
  * @version $Id$
  */
 
+if (!defined('CMSIMPLE_XH_VERSION')) {
+    header('HTTP/1.0 403 Forbidden');
+    exit;
+}
+
 /* utf-8 marker: äöü */
 require_once $pth['folder']['plugin_classes'] . 'filebrowser_view.php';
 require_once $pth['folder']['plugin_classes'] . 'filebrowser.php';
