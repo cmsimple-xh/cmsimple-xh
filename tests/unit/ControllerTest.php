81f0946dba7f712388216b756a7dd6f85ac4f218
cmb69 <cmbecker69@gmx.de>
2014-09-25 19:00:46 +0000

    fixed broken unit tests
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/trunk@1390 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- tests/unit/ControllerTest.php
+++ tests/unit/ControllerTest.php
@@ -1462,10 +1462,11 @@ class ControllerSavePageDataTest extends PHPUnit_Framework_TestCase
      *
      * @global int               The index of the currently selected page.
      * @global XH_PageDataRouter The page data router.
+     * @global XH_CSRFProtection The CSRF protector.
      */
     public function setUp()
     {
-        global $s, $pd_router;
+        global $s, $pd_router, $_XH_csrfProtection;
 
         $_POST = array(
             'foo' => 'bar',
@@ -1473,6 +1474,8 @@ class ControllerSavePageDataTest extends PHPUnit_Framework_TestCase
         );
         $s = 0;
         $pd_router = $this->getMockBuilder('XH_PageDataRouter')
+            ->disableOriginalConstructor()->getMock();
+        $_XH_csrfProtection = $this->getMockBuilder('XH_CSRFProtection')
             ->disableOriginalConstructor()->getMock();
         $this->subject = new XH_Controller();
         $this->eMock = new PHPUnit_Extensions_MockFunction('e', $this->subject);
