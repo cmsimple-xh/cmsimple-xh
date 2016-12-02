a3bc4e095002a11642baf389e210d7e080e75262
cmb69 <cmbecker69@gmx.de>
2016-12-02 10:44:50 +0000

    Fix potentially crashing search
    Due to an issue with ZendLoader, method_exists() can cause a
    segfault. We work around this issue by checking for class_exists()
    first, what is supposed to resolve the problem.
    
    See also <http://cmsimpleforum.com/viewtopic.php?f=10&t=8888>.
    
    git-svn-id: http://svn.code.sf.net/p/cmsimplexh/code/branches/1.6@1682 4f8f9682-67af-44ce-aacd-401fd16d96b5

--- cmsimple/classes/Search.php
+++ cmsimple/classes/Search.php
@@ -93,7 +93,7 @@ class XH_Search
             foreach ($words as $word) {
                 $word = trim($word);
                 if ($word != '') {
-                    if (method_exists('Normalizer', 'normalize')) {
+                    if (class_exists('Normalizer') && method_exists('Normalizer', 'normalize')) {
                         $word = Normalizer::normalize($word);
                     }
                     $this->words[] = $word;
