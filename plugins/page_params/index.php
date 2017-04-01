cmsimple-xh/cmsimple-xh

commit d966f249d64ac98070c5a49486c32cd8db9f8d17

Refactor
We move the call to `Pageparams_handleRelocation()` out of the loop,
because effectively it is a no-op for all pages except `$pd_`, anyway.

plugins/page_params/index.php
@@ -37,14 +37,13 @@ if (!defined('PLUGINLOADER_VERSION')) {
  *
  * @return void
  *
- * @global int    The preliminary index of the current page.
  * @global array  The content of the pages.
  *
  * @since 1.6
  */
 function Pageparams_handleRelocation($index, array $data)
 {
-    global $pd_s, $c;
+    global $c;
 
     $location = $data['header_location'];
     if ((int) $data['use_header_location'] > 0 && trim($location) !== '' ) {
@@ -52,9 +51,7 @@ function Pageparams_handleRelocation($index, array $data)
         if (!$components || !isset($components['scheme'])) {
             $location = CMSIMPLE_URL . $location;
         }
-        if ($index == $pd_s) {
-            $c[$index] = '#CMSimple header("Location:'. $location .'"); exit; #';
-        }
+        $c[$index] = '#CMSimple header("Location:'. $location .'"); exit; #';
     }
 }
 
@@ -201,9 +198,9 @@ if (!$edit && $pd_current) {
  * the currently requested page, a CMSimple script to show a 404 page is added.
  */
 if (!(XH_ADM && $edit)) {
+    Pageparams_handleRelocation($pd_s, $pd_router->find_page($pd_s));
     $temp = $pd_router->find_all();
     foreach ($temp as $i => $j) {
-        Pageparams_handleRelocation($i, $j);
         // unpublishing superseedes hiding:
         if (!Pageparams_isPublished($j)) {
             $c[$i] = '#CMSimple hide#';

commit 3a2846d7623b6a34223ad452849561339bd6d906

Fix regression introduced with commit d966f24
We must not assume that `$pd_s` designates a valid page index; it may
be `-1` as well.

plugins/page_params/index.php
@@ -198,7 +198,9 @@ if (!$edit && $pd_current) {
  * the currently requested page, a CMSimple script to show a 404 page is added.
  */
 if (!(XH_ADM && $edit)) {
-    Pageparams_handleRelocation($pd_s, $pd_router->find_page($pd_s));
+    if ($pd_s >= 0) {
+        Pageparams_handleRelocation($pd_s, $pd_router->find_page($pd_s));
+    }
     $temp = $pd_router->find_all();
     foreach ($temp as $i => $j) {
         // unpublishing superseedes hiding: