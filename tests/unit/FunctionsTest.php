fe53ab85387db479618ed2b3b7e38e437374c7d5
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-18 00:39:22 +0200

    Make test more resilient
    It appears that some ext/intl versions/configurations yield
    `January 2, 1970% at 11:17 AM`, but others `January 2, 1970 11:17 AM`.
    We cater to both variants.

--- tests/unit/FunctionsTest.php
+++ tests/unit/FunctionsTest.php
@@ -693,7 +693,7 @@ class FunctionsTest extends TestCase
         if (class_exists('IntlDateFormatter', false)) {
             $oldLocale = $tx['locale']['all'];
             $tx['locale']['all'] = 'en_US';
-            $this->assertEquals('January 2, 1970 at 11:17 AM', XH_formatDate('123456'));
+            $this->assertStringMatchesFormat('January 2, 1970%s11:17 AM', XH_formatDate('123456'));
             $tx['locale']['all'] = $oldLocale;
         } else {
             $this->assertEquals('January 02, 1970, 11:17', XH_formatDate('123456'));
