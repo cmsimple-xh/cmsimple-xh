298e8b778e09fa6eb1289b3acbcf50806d718224
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-02 14:39:36 +0200

    Fix #265: cmsimple/classes/Menu.php has been removed
    There are several places where it has been documented to include this
    file manually to use `XH_Li`, and since we had renamed the file in
    e4807ea, we had introduced an unintended BC break. Therefore we add the
    file back, so that including it doesn't trigger a fatal error, but
    instead a deprecation notice.
    
    Actually loading `Li.php` as has been suggested, is not necessary, as
    this is catered to by the class autoloader.

---
+++ cmsimple/classes/Menu.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * A dummy file for backward compatibility with CMSimple_XH 1.6
+ */
+
+trigger_error(
+    sprintf('Manual loading of %s is deprecated; use autoloading instead', __FILE__),
+    E_USER_DEPRECATED
+);
