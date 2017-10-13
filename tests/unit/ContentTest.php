2fbfa120d96f8ee799f95d4bfb82fd014675cc84
Christoph M. Becker <cmbecker69@gmx.de>
2017-10-13 15:40:21 +0200

    Fix broken build

--- tests/unit/ContentTest.php
+++ tests/unit/ContentTest.php
@@ -6,7 +6,7 @@ class ContentTest extends TestCase
 {
     /**
      * @return void
-     * 
+     *
      * @see https://github.com/cmsimple-xh/cmsimple-xh/issues/276
      */
     public function testInvalidUtf8IsNotSuppressed()
