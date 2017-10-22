ce39ad9e0d73a155b204fb419a29b7848fb40e4e
Christoph M. Becker <cmbecker69@gmx.de>
2017-10-22 14:54:31 +0200

    Close #321: Filebrowser should use integrated pluginmenu
    Besides that the integrated pluginmenu is a nice alternative to access
    the relevant functionality, it also gives the system some information
    which operations are supported in the plugin back-end.

--- plugins/filebrowser/admin.php
+++ plugins/filebrowser/admin.php
@@ -24,6 +24,8 @@ $_XH_filebrowser->setBrowserPath($pth['folder']['plugins'] . 'filebrowser/');
 $_XH_filebrowser->setMaxFileSize('images', $cf['images']['maxsize']);
 $_XH_filebrowser->setMaxFileSize('downloads', $cf['downloads']['maxsize']);
 
+XH_registerStandardPluginMenuItems(false);
+
 if (XH_wantsPluginAdministration('filebrowser')) {
     $o .= print_plugin_admin('off');
 
