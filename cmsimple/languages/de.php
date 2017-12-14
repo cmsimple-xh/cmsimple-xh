cmsimple-xh/cmsimple-xh

commit 09c1e532ab3e38c7a0c8395f6ce04ea3139be131

Close #116: SystemCheck to state present state
We don't actually change the system check to state the present state,
but rather rework it to make it clear that we're checking for a desired
or required condition, and whether that check succeeded, failed or
raised a warning. This way we can also get rid of the explicit <img>
elements, and instead re-use the respective core classes. We also
refactor a bit.

cmsimple/languages/de.php
@@ -261,22 +261,25 @@ $tx['settings']['warning']="Bitte hier nur solche Änderungen durchführen, bei 
 
 $tx['submenu']['heading']="weiter zu:";
 
-$tx['syscheck']['access_protected']="'%s' zugriffsgeschützt";
-$tx['syscheck']['bom']="Ohne <a href=\"http://www.cmsimple-xh.org/wiki/doku.php/de:utf8#was_ist_ein_bom\" target=\"_blank\">BOM</a>";
-$tx['syscheck']['encoding']="UTF-8-Kodierung eingestellt";
-$tx['syscheck']['extension']="Erweiterung '%s' geladen";
+$tx['syscheck']['access_protected']="'%s' zugriffsgeschützt ist";
+$tx['syscheck']['bom']="kein <a href=\"http://www.cmsimple-xh.org/wiki/doku.php/de:utf8#was_ist_ein_bom\" target=\"_blank\">BOM</a> vorhanden ist";
+$tx['syscheck']['extension']="die Erweiterung '%s' geladen ist";
 $tx['syscheck']['fail']="Fehler";
-$tx['syscheck']['fsockopen']="Funktion fsockopen verfügbar";
-$tx['syscheck']['locale_available']="Locale '%s' verfügbar";
-$tx['syscheck']['locale_default']="Standard-Locale aktiv";
-$tx['syscheck']['magic_quotes']="'Magic quotes runtime' aus";
-$tx['syscheck']['password']="Voreingestelltes Passwort geändert";
-$tx['syscheck']['phpversion']="PHP-Version ≥ %s";
+$tx['syscheck']['fsockopen']="die Funktion fsockopen verfügbar ist";
+$tx['syscheck']['locale_available']="Locale '%s' verfügbar ist";
+$tx['syscheck']['locale_default']="das Standard-Locale aktiv ist";
+$tx['syscheck']['magic_quotes']="magic_quotes_runtime deaktiviert ist";
+$tx['syscheck']['message']="Prüfe, dass %1\$s … %2\$s";
+$tx['syscheck']['password']="das voreingestellte Passwort geändert wurde";
+$tx['syscheck']['phpversion']="die PHP-Version ≥ %s";
+$tx['syscheck']['safe_mode']="safe_mode deaktiviert ist";
 $tx['syscheck']['success']="OK";
-$tx['syscheck']['timezone']="Zeitzone gültig";
+$tx['syscheck']['timezone']="die Zeitzone gültig ist";
 $tx['syscheck']['title']="System-Prüfung";
+$tx['syscheck']['use_only_cookies']="session.use_only_cookies deaktiviert ist";
+$tx['syscheck']['use_trans_sid']="session.use_trans_sid deaktiviert ist";
 $tx['syscheck']['warning']="Warnung";
-$tx['syscheck']['writable']="'%s' schreibbar";
+$tx['syscheck']['writable']="'%s' schreibbar ist";
 
 $tx['sysinfo']['helplinks']="Hilfe und Informationen";
 $tx['sysinfo']['php_version']="PHP-Version";

cmsimple/languages/en.php
@@ -260,22 +260,25 @@ $tx['settings']['warning']="Only change settings when you understand the effect 
 
 $tx['submenu']['heading']="Submenu";
 
-$tx['syscheck']['access_protected']="'%s' access protected";
-$tx['syscheck']['bom']="Without <a href=\"http://www.cmsimple-xh.org/wiki/doku.php/utf8#what_s_a_bom\" target=\"_blank\">BOM</a>";
-$tx['syscheck']['encoding']="Encoding 'UTF-8' configured";
-$tx['syscheck']['extension']="Extension '%s' loaded";
-$tx['syscheck']['fail']="Failure";
-$tx['syscheck']['fsockopen']="Function fsockopen available";
-$tx['syscheck']['locale_available']="Locale '%s' available";
-$tx['syscheck']['locale_default']="Default locale in use";
-$tx['syscheck']['magic_quotes']="Magic quotes runtime off";
-$tx['syscheck']['password']="Non-default password set";
+$tx['syscheck']['access_protected']="'%s' is access protected";
+$tx['syscheck']['bom']="there is no <a href=\"http://www.cmsimple-xh.org/wiki/doku.php/utf8#what_s_a_bom\" target=\"_blank\">BOM</a>";
+$tx['syscheck']['extension']="extension '%s' is loaded";
+$tx['syscheck']['fail']="failure";
+$tx['syscheck']['fsockopen']="function fsockopen is available";
+$tx['syscheck']['locale_available']="locale '%s' is available";
+$tx['syscheck']['locale_default']="default locale is in use";
+$tx['syscheck']['magic_quotes']="magic_quotes_runtime is off";
+$tx['syscheck']['message']="Checking that %1\$s … %2\$s";
+$tx['syscheck']['password']="non-default password is set";
 $tx['syscheck']['phpversion']="PHP version ≥ %s";
-$tx['syscheck']['success']="OK";
-$tx['syscheck']['timezone']="Time zone valid";
+$tx['syscheck']['safe_mode']="safe_mode is off";
+$tx['syscheck']['success']="okay";
+$tx['syscheck']['timezone']="time zone is valid";
 $tx['syscheck']['title']="System check";
-$tx['syscheck']['warning']="Warning";
-$tx['syscheck']['writable']="'%s' writable";
+$tx['syscheck']['use_only_cookies']="session.use_only_cookies is off";
+$tx['syscheck']['use_trans_sid']="session.use_trans_sid is off";
+$tx['syscheck']['warning']="warning";
+$tx['syscheck']['writable']="'%s' is writable";
 
 $tx['sysinfo']['helplinks']="Info and Help Links";
 $tx['sysinfo']['php_version']="PHP-Version";

assets/css/core.css
@@ -274,6 +274,7 @@
 
 #xh_system_check {
     list-style: none;
+    padding-left: 0;
 }
 
 #xh_system_check li {

cmsimple/adminfuncs.php
@@ -53,7 +53,6 @@ function XH_pluginVersion($plugin)
  *
  * @param array $data The data ;)
  *
- * @global array The paths of system files and folders.
  * @global array The localization of the core.
  *
  * @return string HTML
@@ -64,26 +63,22 @@ function XH_pluginVersion($plugin)
  */
 function XH_systemCheck(array $data)
 {
-    global $pth, $tx;
+    global $tx;
 
     $stx = $tx['syscheck'];
-
-    foreach (array('success', 'warning', 'fail') as $img) {
-        $txt = $stx[$img];
-        $imgs[$img] = '<img src="' . $pth['folder']['corestyle'] . $img . '.png"'
-            . ' alt="' . $txt . '" title="' . $txt . '" width="16" height="16">';
-    }
 
     $o = "<h4>$stx[title]</h4>\n<ul id=\"xh_system_check\">\n";
 
     if (key_exists('phpversion', $data)) {
         $ok = version_compare(PHP_VERSION, $data['phpversion']) >= 0;
-        $o .= '<li>' . $imgs[$ok ? 'success' : 'fail']
-            . sprintf($stx['phpversion'], $data['phpversion']) . "</li>\n";
+        $o .= XH_systemCheckLi(
+            '', $ok ? 'success' : 'fail', 
+            sprintf($stx['phpversion'], $data['phpversion'])
+        );
     }
 
     if (key_exists('extensions', $data)) {
-        $cat = ' class="xh_system_check_cat_start"';
+        $cat = 'xh_system_check_cat_start';
         foreach ($data['extensions'] as $ext) {
             if (is_array($ext)) {
                 $notok = $ext[1] ? 'fail' : 'warning';
@@ -91,15 +86,16 @@ function XH_systemCheck(array $data)
             } else {
                 $notok = 'fail';
             }
-            $o .= '<li' . $cat . '>'
-                . $imgs[extension_loaded($ext) ? 'success' : $notok]
-                . sprintf($stx['extension'], $ext) . "</li>\n";
+            $o .= XH_systemCheckLi(
+                $cat, extension_loaded($ext) ? 'success' : $notok,
+                sprintf($stx['extension'], $ext)
+            );
             $cat = '';
         }
     }
 
     if (key_exists('writable', $data)) {
-        $cat = ' class="xh_system_check_cat_start"';
+        $cat = 'xh_system_check_cat_start';
         foreach ($data['writable'] as $file) {
             if (is_array($file)) {
                 $notok = $file[1] ? 'fail' : 'warning';
@@ -107,18 +103,21 @@ function XH_systemCheck(array $data)
             } else {
                 $notok = 'warning';
             }
-            $o .= '<li' . $cat . '>' . $imgs[is_writable($file) ? 'success' : $notok]
-                . sprintf($stx['writable'], $file) . "</li>\n";
+            $o .= XH_systemCheckLi(
+                $cat, is_writable($file) ? 'success' : $notok,
+                sprintf($stx['writable'], $file)
+            );
             $cat = '';
         }
     }
 
     if (key_exists('other', $data)) {
-        $cat = ' class="xh_system_check_cat_start"';
+        $cat = 'xh_system_check_cat_start';
         foreach ($data['other'] as $check) {
             $notok = $check[1] ? 'fail' : 'warning';
-            $o .= '<li' . $cat . '>' . $imgs[$check[0] ? 'success' : $notok]
-                . $check[2] . "</li>\n";
+            $o .= XH_systemCheckLi(
+                $cat, $check[0] ? 'success' : $notok, $check[2]
+            );
             $cat = '';
         }
     }
@@ -126,6 +125,29 @@ function XH_systemCheck(array $data)
     $o .= "</ul>\n";
 
     return $o;
+}
+
+/**
+ * Returns a single `<li>` of the system check.
+ *
+ * @param string $class A CSS class.
+ * @param string $state A state.
+ * @param string $text  A message text.
+ *
+ * @global array The localization of the core.
+ *
+ * @return string
+ *
+ * @since 1.7.0
+ */
+function XH_systemCheckLi($class, $state, $text)
+{
+    global $tx;
+
+    $class = "class=\"xh_$state $class\"";
+    return "<li $class>"
+        . sprintf($tx['syscheck']['message'], $text, $tx['syscheck'][$state])
+        . "</li>\n";
 }
 
 /**
@@ -241,6 +263,7 @@ function XH_sysinfo()
 
 HTML;
 
+    $stx = $tx['syscheck'];
     $checks = array(
         'phpversion' => '5.3',
         'extensions' => array(
@@ -271,43 +294,43 @@ HTML;
     foreach ($files as $file) {
         $checks['other'][] = array(
             XH_isAccessProtected($file), false,
-            sprintf($tx['syscheck']['access_protected'], $file)
+            sprintf($stx['access_protected'], $file)
         );
     }
     if ($tx['locale']['all'] == '') {
-        $checks['other'][] = array(true, false, $tx['syscheck']['locale_default']);
+        $checks['other'][] = array(true, false, $stx['locale_default']);
     } else {
         $checks['other'][] = array(
             setlocale(LC_ALL, $tx['locale']['all']), false,
-            sprintf($tx['syscheck']['locale_available'], $tx['locale']['all'])
+            sprintf($stx['locale_available'], $tx['locale']['all'])
         );
     }
     $checks['other'][] = array(
         date_default_timezone_get() !== 'UTC',
-        false, $tx['syscheck']['timezone']
+        false, $stx['timezone']
     );
     $checks['other'][] = array(
-        !get_magic_quotes_runtime(), false, $tx['syscheck']['magic_quotes']
+        !get_magic_quotes_runtime(), false, $stx['magic_quotes']
     );
     $checks['other'][] = array(
-        !ini_get('safe_mode'), false, 'safe_mode off'
+        !ini_get('safe_mode'), false, $stx['safe_mode']
     );
     $checks['other'][] = array(
-        !ini_get('session.use_trans_sid'), false, 'session.use_trans_sid off'
+        !ini_get('session.use_trans_sid'), false, $stx['use_trans_sid']
     );
     $checks['other'][] = array(
-        ini_get('session.use_only_cookies'), false, 'session.use_only_cookies on'
+        ini_get('session.use_only_cookies'), false, $stx['use_only_cookies']
     );
     $checks['other'][] = array(
         strpos(ob_get_contents(), "\xEF\xBB\xBF") !== 0,
-        false, $tx['syscheck']['bom']
+        false, $stx['bom']
     );
     $checks['other'][] = array(
         !$xh_hasher->checkPassword('test', $cf['security']['password']),
-        false, $tx['syscheck']['password']
+        false, $stx['password']
     );
     $checks['other'][] = array(
-        function_exists('fsockopen'), false, $tx['syscheck']['fsockopen']
+        function_exists('fsockopen'), false, $stx['fsockopen']
     );
     $o .= XH_systemCheck($checks);
     return $o;

commit 528526135954de94ad52fe65b55fff6bc4a9dbf6

Fix #345: System check for session.use_only_cookies is inverted
We fix the regression which we have introduced in
09c1e532ab3e38c7a0c8395f6ce04ea3139be131.

cmsimple/languages/de.php
@@ -281,7 +281,7 @@ $tx['syscheck']['safe_mode']="safe_mode deaktiviert ist";
 $tx['syscheck']['success']="OK";
 $tx['syscheck']['timezone']="die Zeitzone gültig ist";
 $tx['syscheck']['title']="System-Prüfung";
-$tx['syscheck']['use_only_cookies']="session.use_only_cookies deaktiviert ist";
+$tx['syscheck']['use_only_cookies']="session.use_only_cookies aktiviert ist";
 $tx['syscheck']['use_trans_sid']="session.use_trans_sid deaktiviert ist";
 $tx['syscheck']['warning']="Warnung";
 $tx['syscheck']['writable']="'%s' schreibbar ist";

cmsimple/languages/en.php
@@ -280,7 +280,7 @@ $tx['syscheck']['safe_mode']="safe_mode is off";
 $tx['syscheck']['success']="okay";
 $tx['syscheck']['timezone']="time zone is valid";
 $tx['syscheck']['title']="System check";
-$tx['syscheck']['use_only_cookies']="session.use_only_cookies is off";
+$tx['syscheck']['use_only_cookies']="session.use_only_cookies is on";
 $tx['syscheck']['use_trans_sid']="session.use_trans_sid is off";
 $tx['syscheck']['warning']="warning";
 $tx['syscheck']['writable']="'%s' is writable";