a9ec9806cc4184b2e6861875a1e273e0ad802f9a
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2019-12-09 15:10:59 +0100

    Update tplfuncs.php

--- cmsimple/tplfuncs.php
+++ cmsimple/tplfuncs.php
@@ -407,26 +407,26 @@ function lastupdate($br = null, $hour = null)
  *
  * @return string HTML
  */
-function locator()
+function locator($bc_sep = '&gt;')
 {
     $breadcrumbs = XH_getLocatorModel();
     $last = count($breadcrumbs) - 1;
-    $html = '<span vocab="http://schema.org/" typeof="BreadcrumbList">';
+	$html = '<span itemscope itemtype="http://schema.org/BreadcrumbList">';
     foreach ($breadcrumbs as $i => $breadcrumb) {
         list($title, $url) = $breadcrumb;
         if ($i > 0) {
-            $html .= ' &gt; ';
+            $html .= ' ' . $bc_sep . ' ';
         }
-        $html .= '<span property="itemListElement" typeof="ListItem">';
-        $inner = '<span property="name">' . $title
-            . '</span><meta property="position" content="'. ($i + 1) . '">';
+		$html .= '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">';
+		$inner = '<span itemprop="name">' . $title
+			. '</span>';
         if (isset($url) && $i < $last) {
-            $html .= '<a property="item" typeof="WebPage" href="' . $url . '">'
+			$html .= '<a itemprop="item" href="' . $url . '">'
                 . $inner . '</a>';
         } else {
-            $html .= $inner;
+			$html .= $inner;
         }
-        $html .= '</span>';
+		$html .= '<meta itemprop="position" content="'. ($i + 1) . '"></span>';
     }
     $html .= '</span>';
     return $html;
