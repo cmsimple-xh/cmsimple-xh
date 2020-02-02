117986e5fc7b96095ab9d4cff6a763f42c9368a6
manu37 <marinello@pixolution.ch>
2020-02-02 12:00:31 +0100

    update config.php to correct setting "fontawesome"

--- plugins/tinymce4/config/config.php
+++ plugins/tinymce4/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin_cf['tinymce4']['init']="fontawesome-codemirror";
+$plugin_cf['tinymce4']['init']="fontawesome";
 $plugin_cf['tinymce4']['utf8_marker']="äöü";
 $plugin_cf['tinymce4']['CDN_alt_src']="";
 $plugin_cf['tinymce4']['CDN']="";
