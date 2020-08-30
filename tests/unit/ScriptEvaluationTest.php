c036ac28d7332a7e16dda1fc05d8ce0efdfa5ed5
Christoph M. Becker <cmbecker69@gmx.de>
2020-08-30 23:35:12 +0200

    Drop development support for PHP < 7.1.3
    Some of our development requirements are moving very fast, particularly
    PHPUnit does so.  To somewhat keep up, we have drop development support
    for old PHP versions.  That doesn't mean that it is no longer possible
    to develop on, say, PHP 5.6, but rather that `composer install` and
    `phing` are no longer supported for such environments.  That likely
    does not matter much in practise (presumably almost all devs are on
    somewhat current PHP versions).  The apparently biggest drawback is
    that we can no longer run the CI under PHP 5.6.
    
    Lifting our development requirements this way, allows us to drop
    runkit, and also to add Phan directly to the development requirements.
    Plus we can revert the awkward commit ad587f8[1].
    
    [1] <https://github.com/cmsimple-xh/cmsimple-xh/commit/ad587f855d5605591bda07645ae2de82db6d529f>

--- tests/unit/ScriptEvaluationTest.php
+++ tests/unit/ScriptEvaluationTest.php
@@ -11,7 +11,8 @@
 
 namespace XH;
 
-use PHPUnit_Framework_Error_Warning as Warning;
+use PHPUnit\Framework\Error\Error as Error;
+use PHPUnit\Framework\Error\Warning as Warning;
 
 /**
  * A helper to test multiple evaluation of a function with side effects.
@@ -90,10 +91,7 @@ class ScriptEvaluationTest extends TestCase
 
     public function testEvaluateCmsimpleScriptingParseError()
     {
-        if (PHP_MAJOR_VERSION !== 5) {
-            $this->markTestSkipped();
-        }
-        $this->expectOutputRegex('/^\s*Parse error:/s');
+        $this->expectException(Error::class);
         evaluate_cmsimple_scripting('#CMSimple trim(\');#');
     }
 
@@ -164,10 +162,7 @@ class ScriptEvaluationTest extends TestCase
 
     public function testEvaluatePluginCallParseError()
     {
-        if (PHP_MAJOR_VERSION !== 5) {
-            $this->markTestSkipped();
-        }
-        $this->expectOutputRegex('/^\s*Parse error:/s');
+        $this->expectException(Error::class);
         evaluate_plugincall('{{{trim(\')}}}');
     }
 }
