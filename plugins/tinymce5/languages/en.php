078bd21d57de9a4de0390e947ac41dd916a271d7
manu37 <marinello@pixolution.ch>
2023-03-05 18:05:03 +0100

    Fix #551

--- plugins/tinymce5/languages/en.php
+++ plugins/tinymce5/languages/en.php
@@ -1,6 +1,6 @@
 <?php
 $plugin_tx['tinymce5']['cf_init'] = "The size of the toolbar";
 $plugin_tx['tinymce5']['cf_CDN_alt_src']="alternate CDN Source";
-$plugin_tx['tinymce5']['cf_CDN']="CDN Version if checked";
+$plugin_tx['tinymce5']['cf_CDN']="CDN Version if checked. BE AWARE when using CDN you fetch from a foreign server and leave personal data (IP address) there";
 $plugin_tx['tinymce5']['menu_main']="About...";
 $plugin_tx['tinymce5']['cf_utf-8_marker']="<p>Internal usage. <strong>Do not change!</strong></p>";
