226628ac24f78f245218fc42c4bfc652d71819e5
manu37 <marinello@pixolution.ch>
2023-07-09 14:27:38 +0200

    import tinymce6

---
+++ plugins/tinymce6/config/metaconfig.php
@@ -0,0 +1,5 @@
+<?php
+$plugin_mcf['tinymce6']['init']="function:tinymce_getInits";
+$plugin_mcf['tinymce6']['utf8_marker']="hidden";
+$plugin_mcf['tinymce6']['CDN']="bool";   //"" = locally installed, "CDN" = CDN Variant 
+$plugin_mcf['tinymce6']['CDN_src']="text";
