c1f14d84ea58fdfc7e47960e85012250a76e7695
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2024-07-04 12:33:40 +0200

    Country code Sweden not correct
    https://cmsimpleforum.com/viewtopic.php?f=42&t=20405&start=20#p89144

--- cmsimple/config.php
+++ cmsimple/config.php
@@ -8,7 +8,7 @@ $cf['site']['template']="fhs-simple-2019";
 $cf['site']['timezone']="";
 $cf['site']['compat']="true";
 $cf['language']['default']="en";
-$cf['language']['2nd_lang_names']="cs=Čeština;da=Dansk;de=Deutsch;en=English;es=Español;fi=Suomi;fr=Français;el=Ελληνικά;hu=Magyar;it=Italiano;nl=Nederlands;no=Norsk;pl=Polski;ru=Русский;sv=Svensk;sk=Slovenčina";
+$cf['language']['2nd_lang_names']="cs=Čeština;da=Dansk;de=Deutsch;en=English;es=Español;fi=Suomi;fr=Français;el=Ελληνικά;hu=Magyar;it=Italiano;nl=Nederlands;no=Norsk;pl=Polski;ru=Русский;se=Svensk;sk=Slovenčina";
 $cf['mailform']['email']="";
 $cf['mailform']['captcha']="true";
 $cf['mailform']['lf_only']="";
