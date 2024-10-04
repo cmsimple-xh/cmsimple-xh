07d93d7cd9c6c27f826704582ea8cf1917750ca3
manu37 <marinello@pixolution.ch>
2024-10-04 17:01:18 +0200

    Adapt options to tinymce6

--- plugins/tinymce6/config/config.php
+++ plugins/tinymce6/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin_cf['tinymce6']['init']="dropdown-codemirror";
+$plugin_cf['tinymce6']['init']="dropdown";
 $plugin_cf['tinymce6']['utf8_marker']="äöü";
 $plugin_cf['tinymce6']['CDN_src']="https://cdn.tiny.cloud/1/no-api-key/6/tinymce.min.js";
 $plugin_cf['tinymce6']['CDN']="";
