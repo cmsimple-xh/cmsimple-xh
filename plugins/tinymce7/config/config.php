eceff10b582adc7c35afb1ea7d8b6e4e618d45ba
manu37 <marinello@pixolution.ch>
2024-10-10 09:24:22 +0200

    include tinymce7 template plugin removed (Premium since v6)

---
+++ plugins/tinymce7/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$plugin_cf['tinymce7']['init']="dropdown";
+$plugin_cf['tinymce7']['utf8_marker']="äöü";
+$plugin_cf['tinymce7']['CDN_src']="https://cdn.tiny.cloud/1/no-api-key/6/tinymce.min.js";
+$plugin_cf['tinymce7']['CDN']="";
