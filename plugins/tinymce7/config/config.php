bbf5618aa02ed6a413f3148225a357ac45c8d4a8
manu37 <marinello@pixolution.ch>
2024-10-13 10:15:23 +0200

    config/config.php: set CDN source up to date

--- plugins/tinymce7/config/config.php
+++ plugins/tinymce7/config/config.php
@@ -2,5 +2,5 @@
 
 $plugin_cf['tinymce7']['init']="dropdown";
 $plugin_cf['tinymce7']['utf8_marker']="äöü";
-$plugin_cf['tinymce7']['CDN_src']="https://cdn.tiny.cloud/1/no-api-key/6/tinymce.min.js";
+$plugin_cf['tinymce7']['CDN_src']="https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js";
 $plugin_cf['tinymce7']['CDN']="";
