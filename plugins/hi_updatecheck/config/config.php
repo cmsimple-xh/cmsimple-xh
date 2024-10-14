ca31b5b2c2e5d65e83455f3d78529a57255f5144
manu37 <marinello@pixolution.ch>
2024-10-14 08:28:35 +0200

    set config.php_ignore up to date

--- plugins/hi_updatecheck/config/config.php
+++ plugins/hi_updatecheck/config/config.php
@@ -3,6 +3,6 @@
 $plugin_cf['hi_updatecheck']['autocheck']="true";
 $plugin_cf['hi_updatecheck']['autocheck_timeout']="5";
 $plugin_cf['hi_updatecheck']['autocheck_notify']="All updates";
-$plugin_cf['hi_updatecheck']['ignore']="filebrowser,meta_tags,page_params,tinymce,tinymce4,tinymce5,tinymce6";
+$plugin_cf['hi_updatecheck']['ignore']="filebrowser,meta_tags,page_params,tinymce5,tinymce7";
 $plugin_cf['hi_updatecheck']['curl_connect_timeout']="3";
 $plugin_cf['hi_updatecheck']['curl_maxredir']="3";
