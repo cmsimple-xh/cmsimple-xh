0051b29168083b2787dc7164e86e6ce46c681a2c
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2024-11-16 12:16:58 +0100

    Missing defaultconfig.php for hi_updatecheck

---
+++ plugins/hi_updatecheck/config/defaultconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugin_cf['hi_updatecheck']['autocheck']="true";
+$plugin_cf['hi_updatecheck']['autocheck_timeout']="5";
+$plugin_cf['hi_updatecheck']['autocheck_notify']="All updates";
+$plugin_cf['hi_updatecheck']['ignore']="filebrowser,meta_tags,page_params,tinymce4,tinymce5,tinymce7";
+$plugin_cf['hi_updatecheck']['curl_connect_timeout']="3";
+$plugin_cf['hi_updatecheck']['curl_maxredir']="3";
