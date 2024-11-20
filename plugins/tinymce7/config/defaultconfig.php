425e44aabfc36ff4cd1a5d8287776705b4d8b277
manu37 <marinello@pixolution.ch>
2024-11-20 08:13:18 +0100

    set tiny7 defaultconfig according to config

--- plugins/tinymce7/config/defaultconfig.php
+++ plugins/tinymce7/config/defaultconfig.php
@@ -1,5 +1,6 @@
 <?php
-$plugin_cf['tinymce7']['init']="full";
+
+$plugin_cf['tinymce7']['init']="dropdown";
 $plugin_cf['tinymce7']['utf8_marker']="äöü";
 $plugin_cf['tinymce7']['CDN_src']="https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js";
-$plugin_cf['tinymce7']['CDN']="";   //"" = locally installed, "true" = CDN Variant 
+$plugin_cf['tinymce7']['CDN']="";
