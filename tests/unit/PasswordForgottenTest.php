cmsimple-xh/cmsimple-xh

commit 3f6f770f27c4d3482d8e1205f12705915f81f280

Rehash password if necessary (#656)
* Rehash password if necessary

We're using the default parameters of `PASSWORD_BCRYPT`, and especially
the `cost` parameter increased for more recent PHP versions what is a
consequence of ever increasing computing power.  However, if a password
has been hashed with an older PHP version, or a user reuses a hash that
has been created by an old CMSimple_XH version, the `cost` factor might
be as low as `10` or even only `8`, while PHP 8.4 uses `12` by default.

To cater to this, we check whether the password needs to be re-hashed
on successful login with a non-default password, and if so, re-hash the
password.  To be able to actually store the new hash in config.php, we
move the respective method from `ChangePassword` to the `Controller` so
we can re-use the non trivial logic.

* Avoid warning `Undefined array key "xh_default_password"`

When we save the configuration, we need to set the respective session
variable.

An alternative would be to check whether the session variable is set
before accessing it in `XH_writeFile()`.

* Use fix cost of 12 for password hashing

Otherwise password will be re-hashed for older PHP versions (i.e. prior
to PHP 8.4.0), thus weakening brute-force resilience.

* Restore config.php

cmsimple/classes/PasswordForgotten.php
@@ -140,7 +140,7 @@ class PasswordForgotten
         global $pth, $cf, $tx, $e;
 
         $password = bin2hex(random_bytes(8));
-        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
         if (($hash !== false) && ($password != '')) {
             $to = $cf['security']['email'];
             $message = $tx['password_forgotten']['email2_text'] . ' ' . $password;

cmsimple/functions.php
@@ -1445,7 +1445,7 @@ function XH_readFile($filename)
 function XH_writeFile($filename, $contents, $pwChange = false)
 {
     $res = false;
-    if ($_SESSION['xh_default_password'] && !$pwChange) {
+    if (array_key_exists('xh_default_password', $_SESSION) && $_SESSION['xh_default_password'] && !$pwChange) {
         return $res;
     }
     $stream = fopen($filename, 'cb');
@@ -2267,7 +2267,7 @@ function XH_onShutdown()
 {
     global $tx;
 
-    if (!XH_ADM && isset($_SESSION['xh_password'])) {
+    if (defined("XH_ADM") && !XH_ADM && isset($_SESSION['xh_password'])) {
         unset($_SESSION['xh_password']);
     }
 

cmsimple/classes/ChangePassword.php
@@ -152,12 +152,12 @@ class ChangePassword
      */
     public function saveAction()
     {
-        global $o, $pth;
+        global $o, $pth, $_XH_controller;
 
         $this->csrfProtector->check();
         if ($hash = $this->validate($error)) {
             $this->config['security']['password'] = $hash;
-            $this->savePassword();
+            $_XH_controller->saveConfig($pth['file']['config'], $this->config);
             $written = XH_logMessage('info', 'XH', 'login', 'password was changed');
             if (!$written) {
                 e('cntwriteto', 'log', $pth['file']['log']);
@@ -197,45 +197,12 @@ class ChangePassword
                 } elseif ($this->passwordNew != $this->passwordConfirmation) {
                     $error = $this->lang['password']['mismatch'];
                 } else {
-                    $result = password_hash($this->passwordNew, PASSWORD_BCRYPT);
+                    $result = password_hash($this->passwordNew, PASSWORD_BCRYPT, ['cost' => 12]);
                 }
             }
         } else {
             $error = $this->lang['password']['fields_missing'];
         }
         return $result;
-    }
-
-    /**
-     * Saves the configuration with the new password hash.
-     *
-     * @return bool
-     */
-    private function savePassword()
-    {
-        global $pth;
-
-        $o = "<?php\n\n";
-        foreach ($this->config as $cat => $opts) {
-            foreach ($opts as $name => $opt) {
-                // The following are there for backwards compatibility,
-                // and have to be suppressed in the config form.
-                if ($cat == 'security' && $name == 'type'
-                    || $cat == 'scripting' && $name == 'regexp'
-                    || $cat == 'site' && $name == 'title'
-                    || $cat == 'xhtml'
-                ) {
-                    continue;
-                }
-                $opt = addcslashes($opt, "\0..\37\"\$\\");
-                $o .= "\$cf['$cat']['$name']=\"$opt\";\n";
-            }
-        }
-        $o .= "\n?>\n";
-        $res = (bool) XH_writeFile($pth['file']['config'], $o, true);
-        if (function_exists('opcache_invalidate')) {
-            opcache_invalidate($pth['file']['config']);
-        }
-        return $res;
     }
 }

cmsimple/classes/Controller.php
@@ -183,6 +183,14 @@ class Controller
                 $f = 'xh_login_pw_expired';
                 XH_logMessage('warning', 'XH', 'login', 'login password expired');
             } else {
+                if (
+                    $keycut !== 'test'
+                    && password_needs_rehash($cf['security']['password'], PASSWORD_BCRYPT, ['cost' => 12])
+                ) {
+                    $_SESSION['xh_default_password'] = false;
+                    $cf['security']['password'] = password_hash($keycut, PASSWORD_BCRYPT, ['cost' => 12]);
+                    $this->saveConfig($pth['file']['config'], $cf);
+                }
                 setcookie('status', 'adm', 0, CMSIMPLE_ROOT);
                 XH_startSession();
                 session_regenerate_id(true);
@@ -704,5 +712,36 @@ EOT;
             $location = $file . ':' . $line;
             XH_exit(str_replace('{location}', $location, $tx['error']['headers']));
         }
+    }
+
+    /**
+     * Saves the modified configuration $cf
+     *
+     * Occassionally needed, e.g. when storing a new password hash.
+     */
+    public function saveConfig(string $configFile, array $config): bool
+    {
+        $o = "<?php\n\n";
+        foreach ($config as $cat => $opts) {
+            foreach ($opts as $name => $opt) {
+                // The following are there for backwards compatibility,
+                // and have to be suppressed in the config form.
+                if ($cat == 'security' && $name == 'type'
+                    || $cat == 'scripting' && $name == 'regexp'
+                    || $cat == 'site' && $name == 'title'
+                    || $cat == 'xhtml'
+                ) {
+                    continue;
+                }
+                $opt = addcslashes($opt, "\0..\37\"\$\\");
+                $o .= "\$cf['$cat']['$name']=\"$opt\";\n";
+            }
+        }
+        $o .= "\n?>\n";
+        $res = (bool) XH_writeFile($configFile, $o, true);
+        if (function_exists('opcache_invalidate')) {
+            opcache_invalidate($configFile);
+        }
+        return $res;
     }
 }

commit 54aae280b080d0e51fc6b8320ba5c5b8002d8b13

Fix password forgotten feature
This was broken by 3f6f770f27c4d3482d8e1205f12705915f81f280.

Obviously, when resetting the password, there is no session, so PHP 8
will throw a `TypeError` since `$_SESSION` is `null`.  The fix is
trivial, but we take the opportunity to bring that functionality under
test.  To avoid sending an actual mail, we allow to inject a `Mail`
object in the constructor (if none is passed, everything behaves like
before), and stub the `::send()` method.  We then verify that the
password has actually been changed in config.php.

cmsimple/classes/PasswordForgotten.php
@@ -2,6 +2,8 @@
 
 namespace XH;
 
+use XH\Mail;
+
 /**
  * Handling of password forgotten functionality.
  *
@@ -18,6 +20,13 @@ class PasswordForgotten
      * @var string
      */
     private $status = '';
+
+    private ?Mail $mail;
+
+    public function __construct(?Mail $mail = null)
+    {
+        $this->mail = $mail;
+    }
 
     /**
      * Dispatches according to the request.
@@ -112,7 +121,7 @@ class PasswordForgotten
             $message = $tx['password_forgotten']['email1_text'] . "\r\n"
                 . '<' . CMSIMPLE_URL . '?&function=forgotten&xh_code='
                 . $this->mac() . '>';
-            $mail = new Mail();
+            $mail = $this->mail ?? new Mail();
             $mail->setTo($to);
             $mail->setSubject($tx['title']['password_forgotten']);
             $mail->setMessage($message);
@@ -144,7 +153,7 @@ class PasswordForgotten
         if (($hash !== false) && ($password != '')) {
             $to = $cf['security']['email'];
             $message = $tx['password_forgotten']['email2_text'] . ' ' . $password;
-            $mail = new Mail();
+            $mail = $this->mail ?? new Mail();
             $mail->setTo($to);
             $mail->setSubject($tx['title']['password_forgotten']);
             $mail->setMessage($message);

cmsimple/functions.php
@@ -1445,7 +1445,7 @@ function XH_readFile($filename)
 function XH_writeFile($filename, $contents, $pwChange = false)
 {
     $res = false;
-    if (array_key_exists('xh_default_password', $_SESSION) && $_SESSION['xh_default_password'] && !$pwChange) {
+    if (isset($_SESSION['xh_default_password']) && $_SESSION['xh_default_password'] && !$pwChange) {
         return $res;
     }
     $stream = fopen($filename, 'cb');

tests/unit/PasswordForgottenTest.php
@@ -11,6 +11,8 @@
 
 namespace XH;
 
+use org\bovigo\vfs\vfsStream;
+
 /**
  * A test case for the password forgotten class.
  *
@@ -25,15 +27,18 @@ class PasswordForgottenTest extends TestCase
 
     protected function setUp(): void
     {
-        global $cf;
+        global $pth, $cf, $tx;
 
-        $cf = array(
-            'security' => array(
-                'email' => 'devs@cmsimple-xh.org',
-                'secret' => '0123456789abcdef'
-            )
-        );
-        $this->passwordForgotten = new PasswordForgotten();
+        vfsStream::setup('root');
+        $pth['file']['config'] = vfsStream::url('root/config.php');
+        copy('./cmsimple/config.php', vfsStream::url('root/config.php'));
+        $cf = XH_includeVar('./cmsimple/config.php', 'cf');
+        $cf['security']['email'] = 'devs@cmsimple-xh.org';
+        $cf['security']['secret'] = '0123456789abcdef';
+        $tx = XH_includeVar('./cmsimple/languages/en.php', 'tx');
+        $mail = $this->getMockBuilder(Mail::class)->onlyMethods(["send"])->getMock();
+        $mail->expects($this->any())->method("send")->willReturn(true);
+        $this->passwordForgotten = new PasswordForgotten($mail);
     }
 
     protected function currentMac()
@@ -55,5 +60,15 @@ class PasswordForgottenTest extends TestCase
     public function testCheckMac()
     {
         $this->assertTrue($this->passwordForgotten->checkMac($this->currentMac()));
+    }
+
+    public function testSavesNewPasswordOnResetAfterSuccessfulMailing(): void
+    {
+        global $cf;
+        $oldPasswordHash = $cf['security']['password'];
+        $_GET['xh_code'] = '6fa0cd0c8c1f0ba35d37551b2378459c';
+        $this->passwordForgotten->dispatch();
+        $cf = XH_includeVar(vfsStream::url('root/config.php'), 'cf');
+        $this->assertNotEquals($oldPasswordHash, $cf['security']['password']);
     }
 }